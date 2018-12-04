1ff701ee4d710ae167fa3a4f26df676b71fe0e28
rinti <andreas.bernacca@frojd.se>
2018-12-04 22:57:17 +0100

    Add solution for d2p2

--- 02/main.py
+++ 02/main.py
@@ -1,9 +1,10 @@
 from collections import Counter
+from itertools import combinations
 
 with open('./input.txt', 'r') as f:
     lines = list(f.readlines())
     lines = list(map(lambda x: x.rstrip(), lines))
-    lines = list(map(lambda x: Counter(list(x)), lines))
+    counter_lines = list(map(lambda x: Counter(list(x)), lines))
 
     def char_counter(char_count, line):
         return any(list(filter(lambda x: x == char_count, line.values())))
@@ -14,8 +15,22 @@ with open('./input.txt', 'r') as f:
     def char_counter_threes(line):
         return char_counter(3, line)
 
-    threes = len(list(filter(char_counter_threes, lines.copy())))
-    twos = len(list(filter(char_counter_twos, lines.copy())))
+    threes = len(list(filter(char_counter_threes, counter_lines.copy())))
+    twos = len(list(filter(char_counter_twos, counter_lines.copy())))
 
     # First problem:
-    print(twos * threes)
+    #print(twos * threes)
+    
+    for x, y in combinations(lines.copy(), 2):
+        diff = 0
+        for i, char in enumerate(x):
+            if char != y[i]:
+                diff += 1
+
+                if diff > 1:
+                    continue
+
+        if diff == 1:
+            print("".join(
+                a for a, b in zip(x, y) if a == b
+            ))
